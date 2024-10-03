ba0788d888bf63a029b00d54187a087efd915182
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-10-03 11:42:04 +0600

    Initial Commit

---
+++ StickyHeader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StickyHeader
+//
+//  Created by Mohammed Rokon Uddin on 10/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by Mohammed Rokon Uddin on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
